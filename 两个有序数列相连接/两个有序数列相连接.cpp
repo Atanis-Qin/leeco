9b1fdfd50943bad217f05026dbe697b3e5f1b0c9
Atanis Qin <AF19971206H@outlook.com>
2022-02-21 15:43:19 +0800

    test

--- 两个有序数列相连接/两个有序数列相连接.cpp
+++ 两个有序数列相连接/两个有序数列相连接.cpp
@@ -2,20 +2,62 @@
 //
 
 #include <iostream>
+using namespace std;
 
 
 int* test1(int* a, int n, int* b, int m)
 {
     int* newarrayy = new int[n + m]();//加入括号表示全部初始化为0
-    for (int i = 0, j = 0; i < n, j < m; i++, j++)
+    for (int i = 0, j = 0, k = 0; (i < n || j < m) && k < n + m ; k++)
     {
-
+        bool x = a[i] < b[j];
+        newarrayy[k] = x ? a[i] : b[j];
+        if (x)
+        {
+            if (i != n - 1)
+            {
+                i++;
+            }
+            else if (i == n - 1)
+            {
+                for (int ii = j, kk = k+1; ii < m; ii++)
+                {
+                    newarrayy[kk] = b[ii];
+                    kk++;
+                }
+                break;
+            }
+            
+        }
+        else
+        {
+            if (j != m - 1)
+            {
+                j++;
+            }
+            else if (j == m - 1)
+            {
+                for (int jj = i; jj < n; jj++)
+                {
+                    newarrayy[k + 1] = b[jj];
+                }
+                break;
+            }
+        }
     }
+    return  newarrayy;
 }
 
 
 int main()
 {
+    int arr1[] = { 1,3,5,7,9,11,12,13,14,15 };
+    int arr2[] = { 2,4,6,8,10,16,17,19,20 };
+    int* re = test1(arr1, sizeof(arr1) / sizeof(int), arr2, sizeof(arr2) / sizeof(int));
+    for (int i = 0; i < sizeof(arr1) / sizeof(int) + sizeof(arr2) / sizeof(int); i++)
+    {
+        cout << re[i] << endl;
+    }
     std::cout << "Hello World!\n";
 }
 
